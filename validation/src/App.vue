521abc36b8ec702deca88b73ad91cd3ea4ae26cd
Tooj <ganbatshift@gmail.com>
2020-11-30 23:31:24 +0900

    basic form tested

---
+++ validation/src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <div class="form-group" :class="{ 'form-group--error': $v.name.$error }">
+      <label class="form__label">Name</label>
+      <input class="form__input" v-model.trim="$v.name.$model"/>
+    </div>
+    <div class="error" v-if="!$v.name.required">Field is required</div>
+    <div class="error" v-if="!$v.name.minLength">Name must have at least {{$v.name.$params.minLength.min}} letters.</div>
+    <tree-view :data="$v.name" :options="{rootObjectKey: '$v.name', maxDepth: 2}"></tree-view>
+    <div class="form-group" :class="{ 'form-group--error': $v.age.$error }">
+      <label class="form__label">Age</label>
+      <input class="form__input" v-model.trim.lazy="$v.age.$model"/>
+    </div>
+    <div class="error" v-if="!$v.age.between">Must be between {{$v.age.$params.between.min}} and {{$v.age.$params.between.max}}</div><span tabindex="0">Blur to see changes</span>
+    <tree-view :data="$v.age" :options="{rootObjectKey: '$v.age', maxDepth: 2}"></tree-view>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+Vue.use(Vuelidate)
+import { required, minLength, between } from 'vuelidate/lib/validators'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      name: '',
+      age: 0
+    }
+  },
+  validations: {
+    name: {
+      required,
+      minLength: minLength(4)
+    },
+    age: {
+      between: between(20, 30)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
